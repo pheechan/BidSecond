8d8c35be3d455c0189e84a002878f6e5a5d3d370
Akphawee <6540271222@student.chula.ac.th>
2025-05-08 14:24:48 +0700

    test

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION['user'])) {
     header("Location: login.php"); // Redirect to login if not logged in
-    exit();
+    exit();//test
 }
 $user = $_SESSION['user']; // Access user information
 ?>
